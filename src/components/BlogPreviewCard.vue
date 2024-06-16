dcc28bb9f0638a8d5c3c4256bd6d18286fcac36d
Ryan Oliveira <windsofryannn@yahoo.com>
2024-06-16 16:18:24 -0700

    restructured scss files. Created scscc variables file for colors. implemented scss reset file. added support for screen readers. various other improvements

--- src/components/BlogPreviewCard.vue
+++ src/components/BlogPreviewCard.vue
@@ -1,46 +1,58 @@
 <template>
 
+    <h1 class="sr-only">Blog Preview Card Component</h1>
+
     <!-- container -->
-    <div class="container">
+    <main class="container">
+
+        <h2 class="sr-only">Blog Preview Card Details</h2>
 
         <!-- blog svg -->
-        <img class="illustration" :src="cardInfo?.illustration" alt="Blog Illustration" />
+        <img class="illustration" :src="cardInfo?.illustration" alt="Blog card main illustration" />
 
-        <!-- topic tag -->
-        <div class="topic-container">
-            <span class="topic-tag">{{ cardInfo?.tag }}</span>
-        </div>
+        <!-- category tag -->
+        <p class="category-tag" aria-label="category">{{ cardInfo?.tag }}</p>
 
         <!-- publish date -->
-        <div class="publish-container">
-            <span class="publish-date">{{ cardInfo?.publishDate }}</span>
-        </div>
-
+        <p class="publish-date" aria-label="publish date">{{ cardInfo?.publishDate }}</p>
 
         <!-- title -->
-        <div class="title-container">
-            <h2 class="title">{{ cardInfo?.title }}</h2>
-        </div>
+        <h2 class="title">{{ cardInfo?.title }}</h2>
 
         <!-- description -->
         <p class="description-text">{{ cardInfo?.description }}</p>
 
         <!-- author -->
-        <div class="author-container">
-            <img class="avatar" :src="cardInfo?.avatar" alt="Author Avatar"/>
+        <div class="author-container" aria-label="Author information">
+            <img class="avatar" :src="cardInfo?.avatar" alt="Author Avatar" />
             <span class="author-name">{{ cardInfo?.authorName }}</span>
         </div>
-    </div>
+
+    </main>
+
+    <footer role="contentinfo">
+        <span>
+            Frontend Mentor Challenge <a :href="solutionUrl" target="_blank">Social Links Profile</a>.
+            <span class="sr-only">(opens link to new tab)</span>
+        </span>
+        <br />
+        <span>
+            Coded By <a :href="githubUrl" target="_blank">Ryan Oliveira</a>.
+            <span class="sr-only">(opens link to new tab)</span>
+        </span>
+    </footer>
 
 </template>
 
 <script setup lang="ts">
 import { PropType } from 'vue';
-import  { BlogCardInfo }  from '../interfaces/types.ts';
+import { BlogCardInfo } from '../interfaces/types.ts';
 
+const solutionUrl = "https://www.frontendmentor.io/solutions/blog-preview-card-vue-component-written-in-typescript-gjnTDKPrCo";
+const githubUrl = "https://github.com/Ryoliveira"
 
 defineProps({
-    // user PropType when using an interface
+    // use PropType when using an interface
     cardInfo: {
         type: Object as PropType<BlogCardInfo>,
         required: true
